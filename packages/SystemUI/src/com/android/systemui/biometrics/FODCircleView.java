42a101b3dcc978d1c14689037c1c32128da6b892
cfdddd <therandomuser11@gmail.com>
2020-08-17 10:59:52 +0200

    SystemUI: Add FOD Animations
    Adapted to PE's FOD behavior
    Co-authored-by: TheScarastic <warabhishek@gmail.com>
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Marco Rizzo <marco.rizzo.00@outlook.com>
    Signed-off-by: Twel12 <shivansh.aeyron@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -33,6 +33,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
+import android.graphics.drawable.AnimationDrawable;
 import android.hardware.biometrics.BiometricSourceType;
 import android.os.Handler;
 import android.os.Looper;
@@ -46,6 +47,7 @@ import android.provider.Settings;
 import android.net.Uri;
 import android.view.Display;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.View;
@@ -113,6 +115,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
     private Timer mBurnInProtectionTimer;
 
+    private FODAnimation mFODAnimation;
+
     private IFingerprintInscreenCallback mFingerprintInscreenCallback =
             new IFingerprintInscreenCallback.Stub() {
         @Override
@@ -161,7 +165,9 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         public void onKeyguardVisibilityChanged(boolean showing) {
             mIsKeyguard = showing;
             updatePosition();
-            handlePocketManagerCallback(showing);
+            if (mFODAnimation != null) {
+                mFODAnimation.setAnimationKeyguard(showing);
+            }
         }
 
         @Override
@@ -206,6 +212,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
             if (biometricSourceType == BiometricSourceType.FINGERPRINT &&
                     msgId == -1){ // Auth error
                 hideCircle();
+                mHandler.post(() -> mFODAnimation.hideFODanimation());
             }
         }
 
@@ -327,6 +334,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
             throw new RuntimeException("Failed to retrieve FOD circle position or size");
         }
 
+        mFODAnimation = new FODAnimation(context, mPositionX, mPositionY);
+
         Resources res = context.getResources();
 
         mColor = res.getColor(R.color.config_fodColor);
@@ -420,14 +429,17 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
         if (event.getAction() == MotionEvent.ACTION_DOWN && newIsInside) {
             showCircle();
+            mHandler.post(() -> mFODAnimation.showFODanimation());
             return true;
         } else if (event.getAction() == MotionEvent.ACTION_UP) {
             hideCircle();
+            mHandler.post(() -> mFODAnimation.hideFODanimation());
             return true;
         } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
             return true;
         }
 
+        mHandler.post(() -> mFODAnimation.hideFODanimation());
         return false;
     }
 
@@ -591,6 +603,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         if (mIsDreaming) {
             mParams.x += mDreamingOffsetX;
             mParams.y += mDreamingOffsetY;
+            mFODAnimation.updateParams(mParams.y);
         }
 
         mWindowManager.updateViewLayout(this, mParams);
